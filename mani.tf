2039469133cf691743fab9723b04cbf4470c9549
kaseeswarareddy <118353221+kaseeswarareddy@users.noreply.github.com>
2023-01-05 22:49:02 +0530

    change commit

--- mani.tf
+++ mani.tf
@@ -8,10 +8,18 @@ resource "aws_instance" "one" {
 ami = "ami-0cca134ec43cf708f"
 instance_type = "t2.medium"
 tags = {
-Name = "rama"
+Name = "rani"
 }
 }
 
 resource "aws_s3_bucket" "two" {
-bucket = "prave2758"
+bucket = "jyothi123"
+}
+
+resource "aws_ebs_volume" "three" {
+availability_zone = "ap-south-1a"
+size = 40
+tags = {
+  Name = "hello world"
+}
 }
