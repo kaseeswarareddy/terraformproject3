2bbe6ec8f634d627dfeab8d6ef9ba766800c8057
kaseeswarareddy <118353221+kaseeswarareddy@users.noreply.github.com>
2023-01-05 22:27:16 +0530

    upadte commit

--- mani.tf
+++ mani.tf
@@ -1,11 +1,11 @@
 provider "aws" {
-region = "us-east-1"
+region = "ap-south-1"
 access_key = "AKIA5YMYAGZANJQQTYVL"
 secret_key = "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"
 }
 
 resource "aws_instance" "one" {
-ami = "ami-0b5eea76982371e91"
+ami = "ami-0cca134ec43cf708f"
 instance_type = "t2.medium"
 tags = {
 Name = "rama"
